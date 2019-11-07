08677ebdf32dab69b39ec02a2956837beb498859
Shawn Presser <shawnpresser@gmail.com>
2019-11-07 10:47:52 -0800

    WIP float16 support

--- src/model.py
+++ src/model.py
@@ -157,11 +157,9 @@ def model(hparams, X, past=None, scope='model', reuse=tf.AUTO_REUSE):
         batch, sequence = shape_list(X)
 
         wpe = tf.get_variable('wpe', [hparams.n_ctx, hparams.n_embd],
-                             initializer=tf.random_normal_initializer(stddev=0.01),
-                             dtype=dtype)
+                             initializer=tf.random_normal_initializer(stddev=0.01))
         wte = tf.get_variable('wte', [hparams.n_vocab, hparams.n_embd],
-                             initializer=tf.random_normal_initializer(stddev=0.02),
-                             dtype=dtype)
+                             initializer=tf.random_normal_initializer(stddev=0.02))
         past_length = 0 if past is None else tf.shape(past)[-2]
         h = tf.gather(wte, X) + tf.gather(wpe, positions_for(X, past_length))
 
